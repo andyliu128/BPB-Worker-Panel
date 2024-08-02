2ff2d8e37aeaf4a78636ceb0fe0772190217a706
andyliu128 <138580734+andyliu128@users.noreply.github.com>
2024-08-02 14:22:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'cd7055d7-eb5e-42e3-bce0-64e450dab1f7';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
